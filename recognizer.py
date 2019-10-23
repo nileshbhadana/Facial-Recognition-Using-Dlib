40a87396bb4c56ad9a8e91ac7279c68e5939b799
tusshigoyal <56935385+tusshigoyal@users.noreply.github.com>
2019-10-24 00:13:30 +0530

    socket stream update

--- recognizer.py
+++ recognizer.py
@@ -14,6 +14,35 @@ font=cv2.FONT_HERSHEY_SIMPLEX
 with open("encodings.txt",'rb') as file_data:
     known_face_encodings=pickle.load(file_data)
 
+    socket_stream = ssc.socketTextStream("127.0.0.1", 5552)
+
+
+# In[5]:
+
+
+lines = socket_stream.window( 20 )
+
+
+# In[6]:
+
+
+from collections import namedtuple
+fields = ("tag", "count" )
+Tweet = namedtuple( 'Tweet', fields )
+
+
+# In[7]:
+
+
+# Use Parenthesis for multiple lines or use \.
+( lines.flatMap( lambda text: text.split( " " ) ) #Splits to a list
+  .filter( lambda word: word.lower().startswith("#") ) # Checks for hashtag calls
+  .map( lambda word: ( word.lower(), 1 ) ) # Lower cases the word
+  .reduceByKey( lambda a, b: a + b ) # Reduces
+  .map( lambda rec: Tweet( rec[0], rec[1] ) ) # Stores in a Tweet Object
+  .foreachRDD( lambda rdd: rdd.toDF().sort( desc("count") ) # Sorts Them in a DF
+  .limit(10).registerTempTable("tweets") ) ) # Registers to a table.
+
 with open("name.txt",'rb') as file_data:
     known_names=pickle.load(file_data)
 
